5884146dc540d62da86ee94b585616802d2c2d1e
diloes <diloes@hotmail.com>
2023-05-30 19:57:12 +0200

    fix: arrow profile

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,13 @@ const Header = () => {
   return (
     <header className='bg-white py-5 px-20 w-full h-1/3 flex justify-between items-center shadow-md text-gray-500'>
       <div className='flex items-center gap-4'>
-        <Image src='/icon.png' alt='icon' width={40} height={40} className='rounded' />
+        <Image
+          src='/arrow-down.png'
+          alt='icon'
+          width={40}
+          height={40}
+          className='rounded'
+        />
         <nav>
           <ul className='flex items-center gap-4'>
             <li>Empleos</li>
